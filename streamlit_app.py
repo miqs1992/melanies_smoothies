7f6bee7e6f739c44e801b3a3ca1c092908ebf2d6
Wojtek Mikusek <w.mikusek@exlabs.pl>
2024-08-15 18:24:06 +0200

    Update streamlit for remote connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -10,7 +9,8 @@ st.write("Choose the fruits you want in your custom Smoothie!")
 name = st.text_input('Name on Smoothie')
 st.write('Name on your Smoothie will be: ', name)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 ingredient_list = st.multiselect('Choose up to 5 ingredients', my_dataframe, max_selections=5)
